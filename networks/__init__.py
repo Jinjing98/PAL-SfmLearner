c6c5ed62db20c4f7a74a0ff64c7e25d36a93dfc0
jinjing98 <xjj892290531@163.com>
2025-12-16 16:10:19 +0100

    enable use the original DA package for controlled model construction

--- networks/__init__.py
+++ networks/__init__.py
@@ -4,3 +4,5 @@ from .pose_decoder import PoseDecoder
 from .adjust_net import adjust_net
 from .decompose_decoder import decompose_decoder
 from .paba import LocalAffineAlignment
+from .endo_da3 import EndoDepthAnything3Net
+from .endo_vision_transformer import EndoVisionTransformer
